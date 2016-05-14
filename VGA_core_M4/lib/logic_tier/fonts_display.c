8b8930bf229a4efb9bffe7d8718e4e874dc77168
Niek Klaverstijn <niek@klaverstijn.nl>
2016-05-14 17:41:45 +0200

    Added getBitmap function (not tested)

--- VGA_core_M4/lib/logic_tier/fonts_display.c
+++ VGA_core_M4/lib/logic_tier/fonts_display.c
@@ -15,4 +15,37 @@
  ******************************************************************************/
 #include "system.h"
 
+/*******************************************************************************
+ * Functions
+ ******************************************************************************/
+ sChar_t getBitmap(char ascii_char, charSize_t size, charStyle_t style){
+
+		fontStyle_t fontStyle;
+		uint8_t* bitmapArray;
+		sChar_t bitmapData;
+		uint8_t shift;
+
+		if (style == Regular)
+		{
+			if (size == Size_18) fontStyle = FontStyle_Roboto18;
+			if (size == Size_12) fontStyle = FontStyle_Roboto12;
+		}
+		if (style == Bold)
+		{
+			if (size == Size_18) fontStyle = FontStyle_Roboto18Bold;
+			if (size == Size_12) fontStyle = FontStyle_Roboto12;
+		}
+
+		//get address from bitmapram
+		bitmapArray = fontStyle.GlyphBitmaps;
+		//increment pointer to asked ascii char
+		shift = ascii_char-fontStyle.FirstAsciiCode;
+		bitmapData.FirstByte = bitmapArray+shift;
+
+		bitmapData.CharHeight = fontStyle.GlyphHeight;
+		bitmapData.CharWidth = *(fontStyle.GlyphWidth+shift);
+
+		return bitmapData;
+ }
+
  /* End of file fonts_display.c */
--- VGA_core_M4/lib/logic_tier/fonts_display.h
+++ VGA_core_M4/lib/logic_tier/fonts_display.h
@@ -19,12 +19,31 @@ typedef struct {
 	uint8_t * FirstByte;
 	uint8_t CharWidth;
 	uint8_t CharHeight;
-}sChar_t;
+} sChar_t;
 
 /*******************************************************************************
- * Functions
+ * Enumerators
  ******************************************************************************/
+ typedef enum {
+	Size_18,
+	Size_12
+}charSize_t;
+
+ typedef enum {
+	Regular,
+	Bold
+}charStyle_t;
+
+/*******************************************************************************
+ * Function prototypes
+ ******************************************************************************/
+ /**
+  * @brief	Get bitmapdata from memory for ASCII character
+  *
+  * @param size	Select font size as defined in enumerated type charSize_t
+  * @param styleSelect font style as defined in enumerated type charStyle_t
+  * @return	Structure with chardata according to enumerated type sChar_t
+  */
+sChar_t getBitmap(char ascii_char, charSize_t size, charStyle_t style);
 
  /* End of file fonts_display.h */
-
- 
