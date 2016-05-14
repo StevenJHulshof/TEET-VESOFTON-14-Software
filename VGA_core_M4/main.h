c44f58de9288c6852a11012eda6bceaad3451b05
Niek Klaverstijn <niek@klaverstijn.nl>
2016-05-14 13:43:57 +0200

    added headers for files

--- VGA_core_M4/main.h
+++ VGA_core_M4/main.h
@@ -2,12 +2,12 @@
  * Begin of file main.h
  * @author: Lukas ten Hove, Steven Hulshof, Niek Klaverstijn.
  * @version: 1.0
- * Created: 19/04/2016 
+ * Created: 19/04/2016
  *
  *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~**/
 /** @file
  * @brief Contains example layout for header files.
- * 
+ *
  ******************************************************************************/
 
 #ifndef MAIN_H
@@ -17,6 +17,6 @@
  * Includes
  ******************************************************************************/
 #include "stm32f4xx.h"
-
+#include "fontlibrary.h"
 #endif // MAIN_H
 /* End of file main.h */
