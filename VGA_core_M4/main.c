b20ffc54b8d23d005c652daf95f4fb01854e1045
lukashove <lukithove@gmail.com>
2016-05-19 14:40:09 +0200

    define loops removed
    deleted unnecessary includes and emptied main.c

--- VGA_core_M4/main.c
+++ VGA_core_M4/main.c
@@ -28,32 +28,6 @@ int main(void) {
 	SystemInit();
 	UB_VGA_Screen_Init();
 
-	sPosition_t sPos;
-	sBitmap_t sTempchar;
-//
-//		VGA_setPixelData(&sPos, VGA_COL_BLUE);
-	VGA_processScreenData(VGA_COL_RED);
-
-
-	int x,y;
-	for(x = 100, y = 100; x < 200; x++, y++) {
-			sPos.x = x;
-			sPos.y = y;
-			VGA_setPixelData(&sPos, VGA_COL_BLUE);
-
-	}
-
-	sPosition_t sPos2 = {100, 100};
-	color_t clr = VGA_getPixelData(&sPos2);
-	for(x = 200, y = 100; x < 300; x++, y++) {
-				sPos.x = x;
-				sPos.y = y;
-				VGA_setPixelData(&sPos, clr);
-
-		}
-
-	sTempchar = getBitmap('$',Size_18,Regular);
-
 
 	while(1) {
 
