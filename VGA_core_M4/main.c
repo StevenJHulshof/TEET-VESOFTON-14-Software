9ae7cba2f7317eafe202e685ac04983063096d66
Niek Klaverstijn <niek@klaverstijn.nl>
2016-05-25 12:00:59 +0200

    demo with blink addded and vsync

--- VGA_core_M4/main.c
+++ VGA_core_M4/main.c
@@ -16,11 +16,17 @@
  ******************************************************************************/
 #include "vga_lib.h"
 #include "demo.h"
+#include "vga_screen.h"
 
 /*******************************************************************************
  * Main
  ******************************************************************************/
 int main(void) {
+	char text2[] = "Welkom Franc en Michiel!";
+	sPosition_t sPos2 = {80,80};
+	color_t color[2] = {VGA_COL_YELLOW, VGA_COL_BLUE};
+	uint8_t blink = 1;
+	uint8_t delay = 0;
 
 	SystemInit();
 
@@ -37,7 +43,18 @@ int main(void) {
 
 	VGA_D_screenInit();
 
-	while(1);
+	while(1){
+		if(V_SYNC_FLAG)
+		{
+			status = VGA_P_printString(sPos2, text2, Size_18, Bold, color[blink]);
+			if (delay == 10)
+			{
+				blink = !blink;
+				delay=0;
+			}
+			delay++;
+		}
+	}
 	return VGA_SUCCESS;
 }
 /* End of file main.c */
